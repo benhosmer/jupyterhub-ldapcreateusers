c46934fe544a68b879f51a44cef943920face185
Ben Hosmer <ben.hosmer@gmail.com>
2016-09-02 08:51:06 -0400

    A long description helps with pypi page.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 setup(
     name='jupyterhub-ldapacreateusers',
     version='0.1',
-    description='Add on for the ldapauthenticator to create users',
-    url='https://github.com/',
+    description='Add on for the jupyterhub-ldapauthenticator to create system users',
+    long_description='This module requires the jupyterhub-ldapauthenticator module and does nothing\
+                       by itself. jupyterhub-ldapcreateusers allows jupyterhub to create system\
+                       accounts for LDAP authenticated users.',
+    url='https://github.com/benhosmer/jupyterhub-ldapcreateusers',
     author='Ben Hosmer',
     author_email='ben@radarearth.com',
     license='Apache 2',
